8e040d96bbc3f585bda351f99b28922a3487764c
Andrius <andriuscere@gmail.com>
2018-02-02 09:44:27 +0200

    Java0502

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
+
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Iveskite eilučių kiekį ir stulpelių kiekį");
+        Scanner sc = new Scanner(System.in);
+        int eilutes = sc.nextInt();
+        int stulpeliai = sc.nextInt();
+        for (int i = 0; i < eilutes; i++) {
+            for (int j = 0; j < stulpeliai; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("#");
+                } else {
+                    System.out.print("0");
+                }
 
+            }System.out.println();
+        }
     }
 }
